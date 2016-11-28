ae3931e64174bbc643b8f4a3932e2fac1d11c074
Dave Compton <davecompton7@gmail.com>
2016-11-28 21:52:40 +0000

    cleanup

--- Main.hs
+++ Main.hs
@@ -2,16 +2,21 @@
 {-# LANGUAGE RecursiveDo #-}
 import Reflex
 import Reflex.Dom 
-import Data.Map (Map, fromList, elems, insert, delete, empty)
+import Data.Map (Map, fromList, elems)
 import Data.Text (Text, pack)
 import GHCJS.DOM.EventM (mouseOffsetXY) 
 
 type Point = (Int,Int)
 
-type Model = Map Int Point
-
 svgns :: Maybe Text
-svgns = (Just "http://www.w3.org/2000/svg")
+svgns = Just "http://www.w3.org/2000/svg"
+
+boxAttrs :: Map Text Text
+boxAttrs = 
+    fromList [ ("width" , "600")
+             , ("height", "400")
+             , ("style" , "border:solid; margin:8em")
+             ]
 
 pointAttrs :: Point -> Map Text Text
 pointAttrs (x,y) =
@@ -26,25 +31,21 @@ showPoint index point = do
     elStopPropagationNS svgns "g" Mousemove $ 
         elDynAttrNS' svgns "circle" (constDyn $ pointAttrs point) $ return ()
     db <- delay 0.3 =<< getPostBuild
-    return $ ( (fromList $ [(index,Nothing)]) <$ db )
+    return $ fromList [(index,Nothing)] <$ db 
 
 main = mainWidget $ do
-    let attrs = constDyn $ 
-                    fromList [ ("width" , "600")
-                             , ("height", "400")
-                             , ("style" , "border:solid; margin:8em")
-                             ]
-
     rec 
-        (elm, dExpireEvents) <- elDynAttrNS' svgns "svg" attrs $ listHoldWithKey mempty updateEvents showPoint
+        (elm, expireEv) <- elDynAttrNS' svgns "svg" (constDyn boxAttrs) $ listHoldWithKey mempty updateEvents showPoint
 
         traceEvent <- wrapDomEvent (_element_raw elm) (onEventName Mousemove) mouseOffsetXY
 
         dTraceAdd <- foldDyn (\newPos present -> (1 + fst present,newPos)) (0, (0,0)) traceEvent
 
-        let dTraceAddMap = fmap (\(index, pos) -> fromList $ [(index, Just pos)]) dTraceAdd
+        let dTraceAddMap = 
+                fmap (\(index, pos) -> fromList [(index, Just pos)]) dTraceAdd
 
-        let updateEvents = leftmost [ updated dTraceAddMap
-                          , switch $ leftmost.elems <$> current dExpireEvents
-                          ]
+            updateEvents = 
+                leftmost [ updated dTraceAddMap
+                         , switch $ leftmost.elems <$> current expireEv
+                         ]
     return ()
